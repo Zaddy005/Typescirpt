c152205a23988187f115ecc4a9dd73e60ab30f0e
Zaddy <aungnaingphyo005@gmial.com>
2025-02-05 22:54:03 +0630

    5VA

---
+++ part1 basic/l2variables/l2variables.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+var lucknumber = 10;
+console.log(lucknumber);
+lucknumber = 200;
+console.log(lucknumber);
+// Run in terminal ( Convert Typescript to Javascript )
+// tsc l2varialbes.ts
---
+++ part1 basic/l2variables/l2variables.ts
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+
+let lucknumber:number = 10;
+console.log(lucknumber);
+
+lucknumber = 200;
+console.log(lucknumber);
+
+// Run in terminal ( Convert Typescript to Javascript )
+// tsc l2varialbes.ts
